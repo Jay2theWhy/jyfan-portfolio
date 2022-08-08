da94a700ac8bbd71489c41da3f4ef2c03de8ba17
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-08 18:01:33 -0400

    fixing appbar formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import { AppBar, Typography } from '@material-ui/core';
+import { AppBar, Typography, Box, Menu, MenuItem } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <AppBar position="static">
-      <Typography variant="h5">
-        <Link to="/">Home</Link>
-      </Typography>
-      <Typography variant="h5">
-        <Link to="/projects">Projects</Link>
-      </Typography>
-      
+    <AppBar position="static" display="flex">
+      <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+        <Typography variant="h6">
+          <Link to="/">Home</Link>
+        </Typography>
+        <Typography variant="h6">
+          <Link to="/projects">Projects</Link>
+        </Typography>
+      </Box>
+
     </AppBar>
   )
 }
