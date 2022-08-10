174241661f1bb719cf14f3f1f4cb078f81ae5899
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-10 17:19:31 -0400

    add styling for the header section for both desktop and mobile views

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -17,7 +17,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0,1] }}
         transition={{ duration: 1 }}
@@ -61,7 +61,7 @@ const Header = () => {
       {/* images of technologies */}
       <motion.div
         variant={scaleVariants}
-        whileInView={scaleVariants.whilteInView}
+        whileInView={scaleVariants.whileInView}
         className="app__header-circles"
       >
         {[images.flutter, images.redux, images.sass].map((circle, index) => (
