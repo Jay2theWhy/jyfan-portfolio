7dc7cec426e14df4226bb82ca2702d380894413d
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-11 00:26:50 -0400

    import things for work

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
 
       {/* desktop navigation menu */}
       <ul className="app__navbar-links">
-        {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+        {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item) => (
           <li className="app__flex p-text" key={`link-${item}`}>
             <div />
             <a href={`#${item}`}>{item}</a>
@@ -36,7 +36,7 @@ const Navbar = () => {
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
+              {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item) => (
                 <li key={item}>
                   <a href={`#${item}`} onClick={()=> setToggle(false)}>{item}</a>
                 </li>
--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {AiFillEye, AiFillGithub } from 'react-icons/ai';
+import { motion } from 'framer-motion';
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client';
 
 import './Work.scss';
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <>
+    
+    </>
   )
 }
 
-export default Work
+export default AppWrap(Work, 'work')
