f8625e82eac1d7a3cdbc0fd0219b4115a0ca4893
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-10 23:58:11 -0400

    added code to import sanity about descriptions

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,16 +3,25 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
+import { urlFor, client } from '../../client';
 
 // fill in descriptions
-const abouts = [
-  {  title: 'Web Development', description: 'I am a good developer.', imgUrl: images.about01 },
-  {  title: 'Frontend Development', description: 'I am a good developer.', imgUrl: images.about02},
-  {  title: 'Data Science', description: 'I am a good developer.', imgUrl: images.about03},
-  {  title: 'Web Animations', description: 'I am a good developer.', imgUrl: images.about04}
-]
+// const abouts = [
+//   {  title: 'Web Development', description: 'I am a good developer.', imgUrl: images.about01 },
+//   {  title: 'Frontend Development', description: 'I am a good developer.', imgUrl: images.about02},
+//   {  title: 'Data Science', description: 'I am a good developer.', imgUrl: images.about03},
+//   {  title: 'Web Animations', description: 'I am a good developer.', imgUrl: images.about04}
+// ]
 
+// dynamically fetches about from Sanity
 const About = () => {
+  const [abouts, setAbouts] = useState([]);
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+    client.fetch(query)
+      .then((data) => setAbouts(data));
+  }, []);
+
   return (
     <>
       <h2 className="head-text">I Know That <span>Good Development</span><br />means <span>Good Business</span></h2>
@@ -26,7 +35,7 @@ const About = () => {
             className="app__profile-item" 
             key={about.title + index}
           >
-            <img src={about.imgUrl} atl={about.title} />
+            <img src={urlFor(about.imgUrl)} atl={about.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
             <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
           </motion.div>
