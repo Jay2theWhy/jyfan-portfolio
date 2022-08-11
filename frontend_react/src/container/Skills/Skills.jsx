9f2295f8af991ad85b8f2462e3fd94d94075aa1b
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-11 12:11:40 -0400

    import libraries for skills container

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,4 +1,9 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion';
+import ReactTooltip from 'react-tool';
+
+import { AppWrap } from '../../wrapper';
+import { urlFor, client } from '../../client'; 
 
 import './Skills.scss';
 
