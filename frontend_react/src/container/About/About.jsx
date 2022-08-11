f20e29e45c6969c037cac4522de554283edb2ded
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-10 23:31:46 -0400

    added template for about page

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion';
 
+import { images } from '../../constants';
 import './About.scss';
+
+// fill in descriptions
+const abouts = [
+  {  title: 'Web Development', description: 'I am a good developer.', imgUrl: images.about01 },
+  {  title: 'Frontend Development', description: 'I am a good developer.', imgUrl: images.about02},
+  {  title: 'Data Science', description: 'I am a good developer.', imgUrl: images.about03},
+  {  title: 'Web Animations', description: 'I am a good developer.', imgUrl: images.about04}
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">I Know That <span>Good Development</span><br />means <span>Good Business</span></h2>
+      
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration:0.5, type: 'tween' }}
+            className="app__profile-item" 
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} atl={about.title} />
+            <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+          </motion.div>
+        ))}
+
+      </div>
+    </>
   )
 }
 
