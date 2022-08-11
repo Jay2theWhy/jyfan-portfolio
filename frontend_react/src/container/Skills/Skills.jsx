175969c131fb2354513eece9db5a6a47acfcb3e3
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-11 16:47:22 -0400

    fixed quick bug with react-tooltip

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion';
-import ReactTooltip from 'react-tool';
+import ReactTooltip from 'react-tooltip';
 
 import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client'; 
