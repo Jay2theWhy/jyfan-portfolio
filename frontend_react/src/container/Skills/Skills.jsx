d786eb3441e910443427ca984f4849a379573cfa
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-11 16:56:57 -0400

    added skills section with icons

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -8,8 +8,47 @@ import { urlFor, client } from '../../client';
 import './Skills.scss';
 
 const Skills = () => {
+  const [experience, setExperience] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+  // get sanity data
+  useEffect(() => {
+    const experienceQuery = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(experienceQuery)
+      .then((data) => {
+        setExperience(data);
+      })
+
+    client.fetch(skillsQuery)
+    .then((data) => {
+      setSkills(data);
+    })
+  }, [])
+
   return (
-    <div>Skills</div>
+    <>
+      <h2 className="head-text">Skills & Experiences</h2>
+
+      <div className="app__skills-container">
+        <motion.div className="app__skills-list">
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0,1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div className="app__flex" style={{ backgroundColor: skill.bgColor }}>
+                <img src={urlFor(skill.icon)} alt={skill.name}/>
+              </div>
+              <p className="p-text">{skill.name}</p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
+    </>
   )
 }
 
