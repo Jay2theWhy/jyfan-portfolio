67c7b519b95999600efce97c4daec671f0a1e35a
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-12 16:26:03 -0400

    style images for brands

--- frontend_react/src/container/Testimonial/Testimonial.jsx
+++ frontend_react/src/container/Testimonial/Testimonial.jsx
@@ -59,7 +59,7 @@ const Testimonial = () => {
         </>
       )}
 
-      <div className="app__testimonials-brands app__flex">
+      <div className="app__testimonial-brands app__flex">
         {brands.map((brand) => (
           <motion.div
             whileInView={{ opacity: [0, 1] }}
