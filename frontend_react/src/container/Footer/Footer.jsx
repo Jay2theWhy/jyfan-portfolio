5acedd9f413aac3460a212e6ea1592d1cfb435a5
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-12 17:48:40 -0400

    fixed a bug with messages

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -59,7 +59,7 @@ const Footer = () => {
               className="p-text"
               placeholder="Your Message"
               value={message}
-              name={message}
+              name="message"
               onChange={handleChangeInput}
             />
           </div>
