9e30915750297b76b4af74f4cdde364991f49f3c
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-11 22:31:08 -0400

    wrap container with motion wrap

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion';
 import ReactTooltip from 'react-tooltip';
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap, MotionWrap } from '../../wrapper';
 import { urlFor, client } from '../../client'; 
 
 import './Skills.scss';
@@ -93,4 +93,8 @@ const Skills = () => {
   )
 }
 
-export default AppWrap(Skills, "skills");
+export default AppWrap(
+  MotionWrap(Skills, 'app__skills'), 
+  "skills",
+  'app_whitebg'
+);
