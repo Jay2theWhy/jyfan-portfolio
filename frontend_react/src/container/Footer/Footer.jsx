9885bf25c67437e9dc30b99e3978892c8bd96c58
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-12 16:42:01 -0400

    import and setup for footer section

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, {useState } from 'react'
 
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
 import './Footer.scss';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <h2 className="head-text">Take a coffee & chat with me</h2>
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(
+  MotionWrap(Footer, 'app__footer'),
+  'contact',
+  'app__whitebg'
+)
