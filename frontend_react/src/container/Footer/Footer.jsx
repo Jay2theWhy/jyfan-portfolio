34546cf5522fe6d9a970d3f944e009e1ada316cd
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-12 17:26:01 -0400

    Added email, phone, and message feature to Contact section

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -9,6 +9,35 @@ const Footer = () => {
   return (
     <>
       <h2 className="head-text">Take a coffee & chat with me</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+          <a href="mailto:jingyangfan16@gmail.com" className="p-text">jingyangfan16@gmail.com</a>
+        </div>
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile" />
+          <a href="tel: +1 (416) 858-1488" className="p-text">+1 (416) 858-1488</a>
+        </div>
+      </div>
+
+      <div className="app__footer-form app__flex">
+        <div className="app__flex">
+          <input className="p-text" type="text" placeholder="Your Name" name="name" value={name} onChange={handleChangeInput} />
+        </div>
+        <div className="app__flex">
+          <input className="p-text" type="email" placeholder="Your Email" name="email" value={email} onChange={handleChangeInput} />
+        </div>
+        <div>
+          <textarea 
+            className="p-text"
+            placeholder="Your Message"
+            value={message}
+            name={message}
+            onChange={handleChangeInput}
+          />
+        </div>
+        <button type="button" className="p-text" onClick={handleSubmit}>Send Message</button>
+      </div>
     </>
   )
 }
