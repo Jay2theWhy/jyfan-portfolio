5ecf5c4d4cf93dc2fb92c7a68329d5cc805bace0
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-15 17:59:29 -0400

    add button to link resume

--- frontend_react/src/components/SocialMedia.jsx
+++ frontend_react/src/components/SocialMedia.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { BsLinkedin, BsTwitter, BsGithub } from 'react-icons/bs';
 import { FaAngellist } from 'react-icons/fa';
+import { IoDocumentAttach } from 'react-icons/io5';
 
 const SocialMedia = () => {
+  const handleClick = () => {
+    window.open('/JY_Resume.pdf');
+    console.log('hello')
+  }
 
   return (
     <div className="app__social">
@@ -11,6 +16,9 @@ const SocialMedia = () => {
         </a>
         <a href="https://www.linkedin.com/in/jingyangfan/" target="_blank">
             <BsLinkedin />
+        </a>
+        <a>
+          <IoDocumentAttach onClick={handleClick}/>
         </a>
         {/* <a>
             <BsTwitter />
