b62315689081cd9a1ce6319356a4a38e906dede4
Jing Yang Fan <jyfan16@hotmail.com>
2022-08-17 16:56:31 -0400

    changed name of file

--- src/container/Header/Header_details.js
+++
@@ -1,7 +0,0 @@
-const info = {
-    name: "Jing Yang Fan",
-    titles: ["Web Develop", "Data Scientist"],
-    about: "blahblahblahblahblah"
-}
-
-export default info
---
+++ src/container/Header/details.js
@@ -0,0 +1,7 @@
+const details = {
+    name: "Jing Yang Fan",
+    titles: ["Web Developer", "Data Scientist"],
+    about: "blahblahblahblahblah"
+}
+
+export default details
