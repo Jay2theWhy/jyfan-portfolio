2c3d8d7d95be4d27ed14537527f124d76c26f768
Jing Yang Fan <jyfan16@hotmail.com>
2022-09-25 14:47:44 -0400

    remove logo on top left

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
+import { motion } from 'framer-motion';
 import React, { useState } from 'react';
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
 
-import { images } from '../../constants';
+// import { images } from '../../constants';
 import './Navbar.scss';
 
 const Navbar = () => {
@@ -10,10 +10,10 @@ const Navbar = () => {
 
   return (
     <nav className="app__navbar">
-      <div className="app__navbar-logo">
-        {/* change this logo to my own */}
+      {/* <div className="app__navbar-logo">
+        change this logo to my own
         <img src={images.logo} alt="logo" />
-      </div>
+      </div> */}
 
       {/* desktop navigation menu */}
       <ul className="app__navbar-links">
